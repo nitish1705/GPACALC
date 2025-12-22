0d3a6657b6b0cfc25d97b5e9f81e92d11b263c8a
Nitish M <mnitish1705@gmail.com>
2025-12-22 22:38:17 +0530

    Initial Commit

---
+++ GPACALC/GPACALCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPACALCApp.swift
+//  GPACALC
+//
+//  Created by Nitish M on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GPACALCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
